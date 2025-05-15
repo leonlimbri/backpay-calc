62ea5b71266c50a839d95c9be7826ec549c75bfb
llimbri <leonlimbri@gmail.com>
2025-05-15 18:30:56 +1000

    Add ico and error handling

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import json, chardet, io
 
 # Simple streamlit app for backpay calculation
+st.set_page_config(page_title="Backpay Calculator", page_icon="🩺")
 st.title("On-Call Allowances Backpay Calculator")
 
 # Find how many different rates level needed? Initially has to be 3
@@ -50,38 +51,41 @@ for file in files:
 
 # Calculate backpay
 if st.button("Calculate"):
-    # Set up the dataframe first
-    fdf=pd.DataFrame()
-    ts_colnames=["Column1"]
+    if len(files):
+        # Set up the dataframe first
+        fdf=pd.DataFrame()
+        ts_colnames=["Column1"]
 
-    for i in range(13):
-        ncolnames=ts_colnames+[f"tt_label_{i+1}", f"tt_qty_{i+1}"]
-        ndf=df_timesheet[ncolnames]
-        ndf.columns=ts_colnames+["label", "qty"]
-        fdf=pd.concat([fdf, ndf.reset_index(drop=True)])
+        for i in range(13):
+            ncolnames=ts_colnames+[f"tt_label_{i+1}", f"tt_qty_{i+1}"]
+            ndf=df_timesheet[ncolnames]
+            ndf.columns=ts_colnames+["label", "qty"]
+            fdf=pd.concat([fdf, ndf.reset_index(drop=True)])
 
-    # Change data type
-    fdf.loc[:,"Column1"]=fdf.Column1.apply(lambda dt: datetime.datetime.strptime(dt, "%d/%m/%Y %I:%M:%S %p").date())
+        # Change data type
+        fdf.loc[:,"Column1"]=fdf.Column1.apply(lambda dt: datetime.datetime.strptime(dt, "%d/%m/%Y %I:%M:%S %p").date())
 
-    edited_df_rate["start_date"]=edited_df_rate.start_date.apply(lambda d: d.date())
+        edited_df_rate["start_date"]=edited_df_rate.start_date.apply(lambda d: d.date())
 
-    # Calculation done here
-    fdf=apply_rates(fdf, edited_df_rate.to_dict("records"))
-    fdf=fdf.query("label in @on_calls_code")
-    all_on_calls, all_allowances=calculate_backpays(fdf)
+        # Calculation done here
+        fdf=apply_rates(fdf, edited_df_rate.to_dict("records"))
+        fdf=fdf.query("label in @on_calls_code")
+        all_on_calls, all_allowances=calculate_backpays(fdf)
 
-    total_allowances=sum(all_allowances)
-    total_allowances_paid=sum(all_on_calls)*current_base_rate/2
-    total_backpays=sum(all_allowances)-total_allowances_paid
+        total_allowances=sum(all_allowances)
+        total_allowances_paid=sum(all_on_calls)*current_base_rate/2
+        total_backpays=sum(all_allowances)-total_allowances_paid
 
-    st.header("Below is summary of your Backpay", divider=True)
-    st.write(f"Total Allowances for period **{fdf.Column1.min().strftime('%d %b %Y')} - {fdf.Column1.max().strftime('%d %b %Y')}**:")
-    for q_notifier, on_call, allowance in zip(q_notifiers, all_on_calls, all_allowances):
-        if on_call:
-            st.write(
-                "You had ", on_call, f"on calls on **{q_notifier}**", f"which equates to _\${allowance}_", 
-                f", however only paid _\${round(on_call*current_base_rate, 2)}_.",
-                f" **Therefore, expect a backpay of _\${round(allowance-on_call*current_base_rate, 2)}_.**"
-            )
-    st.subheader(f"**Total Allowances = \${total_allowances}**")
-    st.subheader(f"**Total Backpays = \${total_backpays}**")
+        st.header("Below is summary of your Backpay", divider=True)
+        st.write(f"Total Allowances for period **{fdf.Column1.min().strftime('%d %b %Y')} - {fdf.Column1.max().strftime('%d %b %Y')}**:")
+        for q_notifier, on_call, allowance in zip(q_notifiers, all_on_calls, all_allowances):
+            if on_call:
+                st.write(
+                    "You had ", on_call, f"on calls on **{q_notifier}**", f"which equates to _\${allowance}_", 
+                    f", however only paid _\${round(on_call*current_base_rate, 2)}_.",
+                    f" **Therefore, expect a backpay of _\${round(allowance-on_call*current_base_rate, 2)}_.**"
+                )
+        st.subheader(f"**Total Allowances = \${total_allowances}**")
+        st.subheader(f"**Total Backpays = \${total_backpays}**")
+    else:
+        st.caption("No files supplied...")
